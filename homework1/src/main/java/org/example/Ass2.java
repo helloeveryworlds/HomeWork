12aec7574bfa2208dac123370774ef39663adbbe
helloeveryworlds <1121636000@qq.com>
2023-01-26 01:55:50 -0500

    initialize intellij and assgiment2

---
+++ homework1/src/main/java/org/example/Ass2.java
@@ -0,0 +1,7 @@
+package org.example;
+//2. Write a program that prints ‘Hello World’ to the screen.
+public class Ass2 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
